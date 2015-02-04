f140a48506d91f8b5e964cee6ea53f13e100f532
Alex Cepoi <alex.cepoi@gmail.com>
2015-02-04 23:53:29 +0000

    automated setup script

---
+++ setup/setup.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+function install_homebrew {
+    trap "{ rm -f brew.rb; }" EXIT
+    curl -L "https://raw.githubusercontent.com/Homebrew/install/master/install" > brew.rb
+    sed -i "" "s;HOMEBREW_PREFIX = .*;HOMEBREW_PREFIX = '/usr/homebrew';" brew.rb
+    ruby brew.rb
+}
+
+function install_bashrc {
+    sudo cp DIR_COLORS /etc/DIR_COLORS
+    sudo cp bashrc.local /etc/bashrc.local
+
+    sudo -H -s "cp bashrc ~/.bashrc"
+    sudo -H -s "cp profile ~/.profile"
+
+    cp bashrc ~/.bashrc
+    cp profile ~/.profile
+}
+
+function install_packages {
+    brew tap thoughtbot/formulae
+    brew install `tr '\n' ' ' < pkg_brew`
+}
+
+function install_dotvim {
+    sudo git clone https://github.com/alexcepoi/dotvim.git /etc/vim
+    sudo git clone https://github.com/gmarik/Vundle.vim.git /etc/vim/bundle/Vundle.vim
+    sudo vim +PluginInstall +qall
+
+    cp vimrc ~/.vimrc
+    sudo -H -s "cp vimrc ~/.vimrc"
+}
+
+function install_dotfiles {
+    cp rcrc ~/.rcrc
+    rcup
+}
+
+# main
+command -v brew &> /dev/null || install_homebrew
+install_bashrc
+install_packages
+[ -d /etc/vim ] || install_dotvim
+install_dotfiles
